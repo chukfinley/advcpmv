d43f52c6ac17fdb85801ee4b8bc65ffc09aa9a26
hackerman <77645077+chukfinley@users.noreply.github.com>
2023-12-15 21:44:00 +0000

    Create bin.sh

---
+++ bin.sh
@@ -0,0 +1,4 @@
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -o /usr/bin/advmv
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -o /usr/bin/advcp
+sudo chmod +x /usr/bin/advmv
+sudo chmod +x /usr/bin/advcp
