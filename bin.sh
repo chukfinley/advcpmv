a4d96564828ccec9acf61907f61bb6f874eccb5b
hackerman <77645077+chukfinley@users.noreply.github.com>
2023-12-15 21:47:28 +0000

    Update bin.sh

--- bin.sh
+++ bin.sh
@@ -1,4 +1,4 @@
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -o /usr/bin/advmv
-sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -o /usr/bin/advcp
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advmv -O /usr/bin/advmv
+sudo wget https://raw.githubusercontent.com/chukfinley/advcpmv/master/bin/advcp -O /usr/bin/advcp
 sudo chmod +x /usr/bin/advmv
 sudo chmod +x /usr/bin/advcp
